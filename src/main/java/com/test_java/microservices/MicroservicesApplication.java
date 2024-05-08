089ce79cbcf49db141ec6022b9e3e830988da423
albanoGonzalez <albanologro@gmail.com>
2024-05-08 18:55:50 +0200

    Prueba

---
+++ src/main/java/com/test_java/microservices/MicroservicesApplication.java
@@ -0,0 +1,22 @@
+package com.test_java.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;  /*crear una api con un get simple para ver que funciona todo*/
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+ 
+
+@RestController
+@SpringBootApplication
+public class MicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+	@GetMapping("/devolvermensaje")
+	public String devolverMensaje(){
+		return "hola";
+	}
+}
+
